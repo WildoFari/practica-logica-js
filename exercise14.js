fa04daf0772e769967144c40aecf435170079b98
WildoDev <wildofari99@gmail.com>
2025-05-20 10:42:51 -0300

    Exercise 14 JS

---
+++ exercise14.js
@@ -0,0 +1,7 @@
+// Dado un string y un número, repetir el string tantas veces como el número indique.
+
+function repetirSting(texto, veces) {
+    return texto.repeat(veces)
+}
+
+console.log(repetirSting("Hola \n", 10))
