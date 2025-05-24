42fbd4c03c02ef85ea183b85f503a8be70c72537
WildoDev <wildofari99@gmail.com>
2025-05-24 01:37:36 -0300

    Exercise 24Js

--- exercise24.js
+++ exercise24.js
@@ -1,1 +1,27 @@
 // Dado un array de enteros y un número, detectar si esa lista es una permutación del 1 al número aportado.
+
+
+function permutacion(lista, numero) {
+    if (!Array.isArray(lista) || typeof numero !== "number") {
+      return "Datos inválidos";
+    }
+  
+    if (lista.length !== numero) {
+      return false;
+    }
+  
+    const conjunto = new Set(lista);
+  
+    if (conjunto.size !== numero) {
+      return false;
+    }
+  
+    for (let i = 1; i <= numero; i++) {
+      if (!conjunto.has(i)) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
