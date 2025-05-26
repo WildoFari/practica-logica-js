786b4fad8f2f1fa4eac42d0ee96d274d7f9a1daa
WildoDev <wildofari99@gmail.com>
2025-05-26 00:02:49 -0300

    Exercise 26 de JS

--- exercise26.js
+++ exercise26.js
@@ -1,1 +1,13 @@
 // Dado un número, mostrar su serie de Fibonacci.
+
+function fibonacci(num) {
+    const serie = [0, 1];
+
+    for(let i = 2; i < num; i++) {
+        const siguiente = serie[i - 1] + serie[i - 2]
+        serie.push(siguiente)
+    }
+    return [serie, serie [serie.length -1]]
+}
+
+console.log(fibonacci(20))
