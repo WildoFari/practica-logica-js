6715d4d18b240f8ee7e8cae8eb2f6a085e876444
WildoDev <wildofari99@gmail.com>
2025-06-09 23:59:17 -0300

    Exercise 43 JS

--- exercise43.js
+++ exercise43.js
@@ -1,1 +1,13 @@
 // Dado un array de números, devolver la media de todos ellos.
+
+function media(numeros) {
+    if (!Array.isArray(numeros) || numeros.length === 0) {
+      return "Ingresa un array válido";
+    }
+  
+    const soloNumeros = numeros.filter(n => typeof n === "number");
+    const suma = soloNumeros.reduce((acc, n) => acc + n, 0);
+    return suma / soloNumeros.length;
+  }
+
+  console.log(media([1, 2, 3, 4, 5]))
