7884ea4570fdaf525e155194d813492390bb38a0
WildoDev <wildofari99@gmail.com>
2025-06-11 00:00:12 -0300

    Exercise 44 js

--- exercise44.js
+++ exercise44.js
@@ -1,2 +1,11 @@
 // Dado un array de strings, devolver otro array 
 // solo con los elementos que tienen más de dos palabras.
+
+function filtrarFrasesLargas(arr) {
+    return arr.filter(frase => {
+      const palabras = frase.trim().split(/\s+/); 
+      return palabras.length > 2;
+    });
+  }
+  
+  console.log(filtrarFrasesLargas(["hola mundo", "esto es una frase larga", "otra frase corta"]));
