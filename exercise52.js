58c3b10e011809abb0ab8eab14fd4ae678cb148e
WildoDev <wildofari99@gmail.com>
2025-07-14 23:47:18 -0300

    Exercise 52 JS

---
+++ exercise52.js
@@ -0,0 +1,15 @@
+// EJERCICIO 52: SUMA DE IMPARES
+
+// Dado un array de números, devolver la suma de todos los impares.
+
+function sumaImpares(arr) {
+    let suma = 0;
+    for (let num of arr) {
+        if (num % 2 !== 0) {
+            suma += num;
+        }
+    }
+    return suma;
+}
+
+console.log(sumaImpares([1, 2, 3, 4, 5, 6])); // 9
